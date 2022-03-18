e18543e2bda752391e4da559f0b54c6e5019f0c1
Bayja <virlouvetmaxime9@gmail.com>
2022-03-18 10:41:08 +0100

    test

---
+++ Projet_assurance_auto_base/src/test/java/testAssuranceAutoV0/Jeune_Paris_Bonus_Assurance_au_tiers_TARIF.java
@@ -0,0 +1,44 @@
+package testAssuranceAutoV0;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Collection;
+
+import static keyword.Keyword.*;
+import static java.util.Arrays.asList;
+
+@RunWith(Parameterized.class)
+public class Jeune_Paris_Bonus_Assurance_au_tiers_TARIF {
+
+    private final String Vehicule_carburant;
+    private final String Vehicule_marque;
+    private final int Vehicule_modele;
+
+    public Jeune_Paris_Bonus_Assurance_au_tiers_TARIF(final String Vehicule_carburant, final String Vehicule_marque, final int Vehicule_modele) {
+        this.Vehicule_carburant = Vehicule_carburant;
+        this.Vehicule_marque = Vehicule_marque;
+        this.Vehicule_modele = Vehicule_modele;
+    }
+
+    @Parameters(name = "{0}-{1}-{2}")
+    public static Collection<Object[]> dataSets() {
+        return asList(new Object[][]{
+            {"Diesel", "Peugeot", 308}
+        });
+    }
+
+    @Before
+    public void setUp() {
+        ouvrirNavigateur(https://demo-simulator.herokuapp.com/);
+    }
+
+
+    @Test
+    public void execute() {
+        saisirCaracteristiquesVehicule(Vehicule_marque, Vehicule_modele, Vehicule_carburant);
+    }
+}
---
+++ Projet_assurance_auto_base/src/test/java/testAssuranceAutoV0/Jeune_Province_MALUS_Assurance_tous_risques_TARIF.java
@@ -0,0 +1,44 @@
+package testAssuranceAutoV0;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Collection;
+
+import static keyword.Keyword.*;
+import static java.util.Arrays.asList;
+
+@RunWith(Parameterized.class)
+public class Jeune_Province_MALUS_Assurance_tous_risques_TARIF {
+
+    private final String Vehicule_carburant;
+    private final String Vehicule_marque;
+    private final String Vehicule_modele;
+
+    public Jeune_Province_MALUS_Assurance_tous_risques_TARIF(final String Vehicule_carburant, final String Vehicule_marque, final String Vehicule_modele) {
+        this.Vehicule_carburant = Vehicule_carburant;
+        this.Vehicule_marque = Vehicule_marque;
+        this.Vehicule_modele = Vehicule_modele;
+    }
+
+    @Parameters(name = "{0}-{1}-{2}")
+    public static Collection<Object[]> dataSets() {
+        return asList(new Object[][]{
+            {"Diesel", "Mercedes Benz", "Class B"}
+        });
+    }
+
+    @Before
+    public void setUp() {
+        ouvrirNavigateur(https://demo-simulator.herokuapp.com/);
+    }
+
+
+    @Test
+    public void execute() {
+        saisirCaracteristiquesVehicule(Vehicule_marque, Vehicule_modele, Vehicule_carburant);
+    }
+}
